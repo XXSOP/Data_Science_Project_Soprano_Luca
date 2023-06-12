d1ef15f39cfe2cbe982635703331d68edc6a4784
XXSOP <lucasoprano.02@gmail.com>
2023-06-12 17:29:51 +0200

    Continuazione stesura presentazione

--- Grafo_1.R
+++ Grafo_1.R
@@ -4,43 +4,45 @@ library(ggplot2)
 library(ggmap)
 library(tidyr)
 
-#Dataset di partenza
+#Dataset originali di partenza
 map.world = map_data("world")
 originalDataset = read.csv("mission_launches.csv")
 
+#Serie di mutate per estrarre il nome della nazione 
 df = originalDataset %>%
   mutate(Country =str_split_i(originalDataset$Location, ',',-1))%>%
   mutate(Country = str_trim(Country))
 
-
+#Conta il numero di volte che un paese compare nel dataset e li ordina in modo decrescente
 df_country = df %>%
   group_by(Country) %>%
   summarise(n = n()) %>%
   arrange(-n)
 
 
-#left join to obtain the longitude and latitude in the library map_data("world") for the geom_polygon
+#left join per ottenere la longitudine e latitudine nella libreria map_data("world") per il geom_polygon
 map.world_joined = left_join( map.world, df_country,  by = c( 'region' = 'Country'))
 map.world_joined = map.world_joined %>%
-  mutate( n = replace_na(n, 0)) #replace the na value with a 0, the value n if is different from 0
+  mutate( n = replace_na(n, 0)) #rimpiazza il valore na con uno 0, usa il valoe n se è diverso da 0#
 
-
+#Cambio il tipo dei dati nella colonna n creata col mutate
 map.world_joined$n = as.numeric(map.world_joined$n)
 
 
+#Plotta il dataset map.world_joined creando una cartina geografica usando longitudine e latitudine 
+# colorando i paesi in colori diversi in base a quante volte compaiono nel dataset (ovvero hanno eseguito diversi lanci missilistici)
 ggplot() +
   geom_polygon(data = map.world_joined, aes(x = long, y = lat, group = group, fill = n)) +
   scale_fill_gradient(low = "#abdbe3", high = "#154c79") +
   labs(title = "Lanci missilistici nel mondo", y="Latitudine", x="Longitudine") +
-  theme(text = element_text(family = "Times New Roman", color = "black",)
+  theme(text = element_text(color = "black",)
     ,panel.background = element_rect(fill ="#FaFFFF")
     ,plot.background = element_rect(fill = "white")
     ,panel.grid = element_blank()
-    ,plot.title = element_text(size = 30)
-    ,plot.subtitle = element_text(size = 10)
-    ,axis.text = element_blank()
-    ,axis.ticks = element_blank()
-    ,axis.title.x = element_text(size = 20)
-    ,axis.title.y = element_text(size = 20)
+    ,plot.title = element_text(size = 20)
+    #,axis.text = element_blank()
+    #,axis.ticks = element_blank()
+    ,axis.title.x = element_text(size = 10)
+    ,axis.title.y = element_text(size = 10)
     ,legend.position = "none"
   )
--- Grafo_2.R
+++ Grafo_2.R
@@ -2,28 +2,33 @@ library(dplyr)
 library(stringr)
 library(ggplot2)
 
+#Dataset di partenza
 originalDataset = read.csv("mission_launches.csv")
 
+#Tramite una serie di mutate estrae l'anno dalla colonna Date
 df1=originalDataset %>%
   mutate(Year =str_split_i(Date, ',',-1))%>%
   mutate(Year= str_trim(Year))%>%
   mutate(Year =str_split_i(Year, ' ',1))
 
+#Raggruppo Mission_Status e Year e li conto così da avere per ogni anno quanto stati di missione di missione ci sono stati
 df2= df1%>%
   group_by(Mission_Status, Year)%>%
   summarise(n = n())
 
+#Plotta il subset df2 mappando sulle x gli anni estratti nelle righe dalla 9-12 e sulle y mappa 
+# il conto dei vari stati delle missioni
 ggplot(df2, mapping = aes(x= Year , y= n))+
-  geom_bar(stat = "identity", aes(fill = Mission_Status), width = 0.8, position = position_stack())+
+  geom_bar(stat = "identity", aes(fill = Mission_Status), width = 0.8, position = position_stack())+ #Colora le varie barre del barplot diversamente in base allo stato delle missioni
   labs(title = "Numero di stati che le missioni hanno assunto",
-    y="Numero di occorrenze", x="Anni analizzati")+
-  theme(text = element_text(color = "black")
+    y="Numero di occorrenze", x="Anni analizzati (1957-2020)")+
+  theme(text = element_text(color = "black") #tra le parentesi del theme() ci sono degli elementi che rendono il grafico più bello e leggibile
     ,panel.background = element_rect(fill ="#FaFFFF")
     ,plot.background = element_rect(fill = "white")
     ,panel.grid = element_blank()
     ,plot.title = element_text(size = 20)
-    ,axis.ticks = element_blank()
-    ,axis.title.x = element_text(size = 20)
-    ,axis.title.y = element_text(size = 20)
+    ,axis.title.x = element_text(size = 10)
+    ,axis.title.y = element_text(size = 10)
+    ,axis.text.x = element_blank()
   )
 
